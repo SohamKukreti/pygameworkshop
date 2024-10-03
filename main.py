beeaf577398bb109fa10a2a774134bb5d89b5224
Soham Kukreti <kukretisoham@gmail.com>
2024-10-03 20:36:35 +0530

    Add person and movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 pygame.init()
+clock = pygame.time.Clock()
 
 screen_width = 800
 screen_height = 600
@@ -10,12 +11,45 @@ pygame.display.set_caption("OSDC workshop")
 
 running = True
 
-square = pygame.rect.Rect(0, 0 , 50, 50)
+item_x = screen_width // 2
+item_y = screen_height - screen_height // 4
+item_dx = 0
+item_dy = 0
+item = pygame.image.load("assets/person.png")
+item = pygame.transform.scale(item, (58, 148))
+
+def display():
+    screen.fill((255, 255, 255))
+    screen.blit(item, (item_x, item_y))
+    pygame.display.update()
 
 
 while running:
-    screen.fill((255, 255, 255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.exit()
-    pygame.display.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                item_dx += 5
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s: 
+                item_dy += 5
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                item_dx -= 5
+            if event.key == pygame.K_UP or event.key == pygame.K_w: 
+                item_dy -= 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                item_dx -= 5
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                item_dy -= 5
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                item_dx += 5
+            if event.key == pygame.K_UP or event.key == pygame.K_w: 
+                item_dy += 5
+
+    item_x += item_dx
+    item_y += item_dy
+    print(item_dx)
+    print(item_dy)
+    display()
+    clock.tick(60)
